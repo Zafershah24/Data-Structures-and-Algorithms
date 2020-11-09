f943f9826a9b0b3118d88bec2a98d1cb883f4d75
zafer247 <zafershah247@gmail.com>
2020-11-09 22:48:24 +0530

    Implementation of PVF

---
+++ Pure_Virtual_Func.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+
+class Shape {
+    public:
+   virtual void area()=0;
+};
+
+class Circle:public Shape{
+    int r;
+    public:
+    void area(){
+       cout<<"Enter Value of R"<<endl;
+       cin>>r;
+       cout<<"Area of the Circle : "<<3.14*r*r<<endl;
+   }
+};
+class Rectangle:public Shape {
+       int l,b;
+    public:
+    void area(){
+       cout<<"Enter Value of Length and Breadth"<<endl;
+       cin>>l>>b;
+       cout<<"Area of the Rectangle : "<<l*b<<endl;
+   }
+};
+
+int main() {
+    // Write C++ code here
+    Shape *s;
+    Circle c;
+    s=&c;;
+    s->area();
+    Rectangle r;
+    s=&r;
+    s->area();
+
+    return 0;
+}
