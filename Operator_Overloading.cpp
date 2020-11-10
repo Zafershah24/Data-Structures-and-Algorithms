e8d5e2d860895eb68ec9519f55f9746cd29b619b
zafer247 <zafershah247@gmail.com>
2020-11-10 15:46:22 +0530

    Operator Overloading, Increment & Decrement operators

--- Operator_Overloading.cpp
+++ Operator_Overloading.cpp
@@ -15,12 +15,25 @@ class Complex {
             i=b;
         }
         void disp(){
-            cout<<r<<"+"<<i<<"i";
+            cout<<r<<"+"<<i<<"i"<<endl;
         }
         Complex operator +(Complex c ){
             Complex t;
             t.r=r+c.r;
             t.i=i+c.i;
+            return t;
+        }
+        
+        Complex operator ++(){
+            Complex t;
+            t.r=++r;
+            t.i=++i;
+            return t;
+        }
+         Complex operator --(){
+            Complex t;
+            t.r=--r;
+            t.i=--i;
             return t;
         }
     
@@ -32,6 +45,11 @@ int main() {
     Complex c3;
     c3=c1+c2;
     c3.disp();
+    ++c3;
+    c3.disp();
+    Complex c4;
+    c4=--c3;
+    c4.disp();
     
 
     return 0;
