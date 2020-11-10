a6a90e9e656f0a73d2e7829aa4e5a9da0a99951f
zafer247 <zafershah247@gmail.com>
2020-11-10 16:02:38 +0530

    Function_Teplate Implementation

---
+++ Function_Template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+template <typename t>
+t add (t a, t b){
+    return a+b;
+}
+
+
+int main() {
+    cout<<add<float>(3.4f,7.6f)<<endl;
+    cout<<add<double>(3.43,7.44)<<endl;
+    cout<<add<int>(3,7)<<endl;
+    
+    
+
+    return 0;
+}
