10ac1e85ecacd1d00eab363e719fddebc298ad54
zafer247 <zafershah247@gmail.com>
2020-11-14 13:07:13 +0530

    Update LinearSearch.cpp

--- Search Algos/LinearSearch.cpp
+++ Search Algos/LinearSearch.cpp
@@ -1,1 +1,41 @@
 
+#include<iostream>
+using namespace std;
+
+LS(int a[],int n){
+   int temp=-1;
+for (int i=0;i<sizeof(a);i++)
+
+{
+
+ {if(a[i]==n)
+cout<<"Element found at pos "<<i+1<<endl;
+temp=0;
+}
+}
+if (temp=-1) cout<<"element not found"<<endl;
+
+
+
+}
+
+
+
+int main(){
+int *a;
+int s;
+int n;
+cout<<"Enter the size of your array"<<endl;
+cin>>s;
+a=new int[s];
+cout<<"Enter the values for the array"<<endl;
+for (int i=0;i<s;i++){
+   cin>> a[i];
+}
+
+cout<<"Enter the value you want to Search for"<<endl;
+cin>>n;
+
+LS(a,n);
+
+}
