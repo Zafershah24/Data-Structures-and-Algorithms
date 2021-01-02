214189f98b984db7ea8fc3209ca1319102c7427a
zafer247 <zafershah247@gmail.com>
2021-01-02 20:46:55 +0530

    recursion implementation

---
+++ recursion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int fact(int a){
+    if(a==1)
+    return 1;
+    else
+    {
+    return a*fact(a-1);
+    }
+}
+
+int main() {
+   
+  cout<<fact(3);
+
+}
